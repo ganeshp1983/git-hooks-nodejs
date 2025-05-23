c251d2705c32d2c32685faf1a9d73909809acc60
ganeshp1983 <ganesh.prasad170683@gmail.com>
2025-05-23 15:29:01 +0530

    Added jtest tests

---
+++ tests/mult.test.js
@@ -0,0 +1,4 @@
+const mult = require('../src/mult');
+it('should return correct multiplication', () => {
+        expect(mult(2, 3)).toBe(6);
+})
---
+++ tests/sum.test.js
@@ -0,0 +1,4 @@
+const sum = require('../src/sum');
+it('should return correct sum', () => {
+        expect(sum(1, 2)).toBe(3);
+})
